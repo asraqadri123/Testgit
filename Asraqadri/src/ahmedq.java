aad10f5f67a7635197e87b3ecf9f8177800fac16
Fatim <Fatim@DESKTOP-A2DQNMP.home>
2018-10-16 23:28:18 -0500

    first commit

---
+++ Asraqadri/src/ahmedq.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class ahmedq {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+		String url = "http://www.faceboook.com";
+		driver.get(url);
+		driver.findElement(By.xpath("//*[@id='email']")).sendKeys("asraqadri1@gmail.com");
+		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("1234");
+		driver.findElement(By.xpath(".//*[@id='loginbutton']")).click();
+		
+		//Click on back button
+		driver.navigate().back();
+		//driver.findElement(By.xpath(".//*[@id='u_0_c']")).sendKeys("asra");
+		//driver.findElement(By.xpath(".//*[@id='u_0_e']")).sendKeys("qadri");
+		//driver.findElement(By.xpath(".//*[@id='u_0_h']")).sendKeys("972-955-7848");
+		//driver.findElement(By.xpath(".//*[@id='u_0_l']")).sendKeys("marium05");
+		
+			
+		}
+
+	}
+
+
